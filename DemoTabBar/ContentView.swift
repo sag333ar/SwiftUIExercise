dc207f6cb185767b50da1c6caf69434ec9f943a5
Sagar Kothari <sag333ar@gmail.com>
2019-07-23 06:33:03 +0530

    created project

---
+++ DemoTabBar/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  DemoTabBar
+//
+//  Created by sagar kothari on 23/07/19.
+//  Copyright © 2019 sagar kothari. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabbedView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
